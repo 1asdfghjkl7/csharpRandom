01643e613865599c3a68dbe195ab28552aed2d1a
Jordan Williams <jordanwilliams288@gmail.com>
2018-08-27 10:08:03 -0500

    finished exercise

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            // Create a list
+            List<int> randList = new List<int>();
+            // Populate the list
+            for (int i = 0; i < 20; i++)
+            {
+                randList.Add(random.Next(1, 51));
+            }
+
+            List<int> sqrList = new List<int>();
+            List<int> sqrListNoOdd = new List<int>();
+
+            foreach (var item in randList)
+            {
+                sqrList.Add(item ^ 2);
+            }
+
+            foreach (var item in sqrList)
+            {
+                if (item % 2 == 0)
+                {
+                    sqrListNoOdd.Add(item);
+                }
+            }
+
+            foreach (var item in sqrListNoOdd)
+            {
+                Console.WriteLine(item);
+
+            }
+
+        }
+    }
+}
